775a176db005dba03f149d455ef046c9d6fe2ee0
spicybyte <onespicybyte@gmail.com>
2018-12-14 17:21:27 -0600

    Add About page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch
+} from "react-router-dom";
 import Landing from "./pages/Landing";
-import NavBar from "./components/NavBar/NavBar";
+import About from "./pages/About/About";
+// import NavBar from "./components/NavBar/NavBar";
 import "./styles/App.sass";
 
 class App extends Component {
@@ -11,7 +17,16 @@ class App extends Component {
         <Switch>
           <Route exact path="/">
             {routeProps => (
-              <Landing 
+              <Landing
+                {...routeProps}
+                // other props can be added here this will allow login/logout logic to live in this component
+                // along with whatever other functionality and the relevant data we want to live in state in this component
+              />
+            )}
+          </Route>
+          <Route exact path="/about">
+            {routeProps => (
+              <About
                 {...routeProps}
                 // other props can be added here this will allow login/logout logic to live in this component
                 // along with whatever other functionality and the relevant data we want to live in state in this component
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,5 @@
-import React, { Fragment } from 'react';
-import { Link } from "react-router-dom"
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
 import "./NavBar.scss";
 
 const NavBar = props => (
@@ -9,32 +9,29 @@ const NavBar = props => (
       <nav className="header__nav">
         <ul>
           <li>
-            <Link to="#">About</Link>
+            <Link to="/about">About</Link>
           </li>
-          {props.loggedIn
-            ? (
-              <Fragment>
-                <li>
-                  <Link to="#">Wishlist</Link>
-                </li>
-                <li>
-                  <Link to="#">Cart</Link>
-                </li>
-                <li>
-                  <Link to="#">Sign Out</Link>
-                </li>
-              </Fragment>
-            ) : (
+          {props.loggedIn ? (
+            <Fragment>
               <li>
-                <Link to="#">Sign In</Link>
+                <Link to="#">Wishlist</Link>
               </li>
-            )
-          }
+              <li>
+                <Link to="#">Cart</Link>
+              </li>
+              <li>
+                <Link to="#">Sign Out</Link>
+              </li>
+            </Fragment>
+          ) : (
+            <li>
+              <Link to="#">Sign In</Link>
+            </li>
+          )}
         </ul>
       </nav>
     </div>
-
-  </header >
+  </header>
 );
 
 export default NavBar;
