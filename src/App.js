cd7c583e50099c073d22a6d51e67c3b762755bfc
spicybyte <onespicybyte@gmail.com>
2018-12-13 22:25:06 -0600

    Install node-sass and text SaSS stylings

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "./styles/App.sass";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>This is App.js stylings using SaSS</p>
         </header>
       </div>
     );
