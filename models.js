3832250de547695e8be5089e31f9c715241fda34
spicybyte <onespicybyte@gmail.com>
2019-01-29 17:32:26 -0600

    Add two images sizes to database

--- models.js
+++ models.js
@@ -1,32 +1,34 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize({
-  database: 'horology_db',
-  dialect: 'postgres',
+  database: "horology_db",
+  dialect: "postgres",
   operatorsAliases: false,
   define: {
     underscored: true
   }
 });
 
-const Watch = sequelize.define('watches', {
+const Watch = sequelize.define("watches", {
   name: Sequelize.STRING,
   description: Sequelize.STRING,
   price: Sequelize.INTEGER,
   image: Sequelize.STRING,
+  image30: Sequelize.STRING,
+  image400: Sequelize.STRING,
   brand: Sequelize.STRING,
   gender: Sequelize.STRING
 });
 
-const User = sequelize.define('users', {
+const User = sequelize.define("users", {
   username: Sequelize.STRING,
   password: Sequelize.STRING,
-  wishlist: Sequelize.STRING,
-  cart: Sequelize.STRING
+  wishlist: Sequelize.TEXT,
+  cart: Sequelize.TEXT
 });
 
 module.exports = {
   sequelize,
   Watch,
   User
-}
+};
