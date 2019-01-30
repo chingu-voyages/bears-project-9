e0d9f96aadfe225d4dc0b25f7ae72b8fe1fb9b35
spicybyte <onespicybyte@gmail.com>
2019-01-29 20:28:56 -0600

    Create Badge component

---
+++ client/src/components/Badge/Badge.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Badge.scss";
+
+class Badge extends React.Component {
+  state = {
+    hovered: false
+  };
+
+  showBadge = () => {
+    this.setState({ hovered: true });
+  };
+  hideBadge = () => {
+    this.setState({ hovered: false });
+  };
+
+  render() {
+    const { children, badgeLabel } = this.props;
+
+    return (
+      <div
+        className="badge-component"
+        onMouseOver={this.showBadge}
+        onMouseOut={this.hideBadge}
+      >
+        {this.state.hovered && (
+          <span className="badge-label">{badgeLabel}</span>
+        )}
+
+        {children}
+      </div>
+    );
+  }
+}
+
+export default Badge;
