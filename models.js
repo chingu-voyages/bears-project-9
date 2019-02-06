939eb4aee993807d0b97c8916379a3815c7d252c
Strangebrewer <BKAShambala@gmail.com>
2019-02-06 02:04:36 -0700

    deploying to see if settings are all correct

--- models.js
+++ models.js
@@ -1,21 +1,20 @@
 const { Sequelize } = require("sequelize");
-// const { Client } = require("pg");
-
-// const client = new Client({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: true
-// });
-// client.connect();
-
+const { Client } = require("pg");
 const env = process.env.NODE_ENV || 'development';
 const config = require("./config.json")[env]
 
-let sequelize;
+if (config.use_env_variable) {
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+  });
+  client.connect();
+}
 
+let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-}
-else {
+} else {
   sequelize = new Sequelize({
     database: "horology_db",
     dialect: "postgres",
