3589e46af52dcb6c52f2c898ecec17ab8e46a95a
Strangebrewer <BKAShambala@gmail.com>
2019-02-06 05:30:41 -0700

    wtf

--- fuct.js
+++ fuct.js
@@ -1,21 +1,28 @@
 const { Sequelize } = require("sequelize");
 
-let connection;
-if (process.env.NODE_ENV === 'production') {
-  console.log("In production!");
-  connection = process.env.DATABASE_URL;
-} else {
-  console.log("Not in production!");
-  connection = {
-    database: "horology_db",
-    dialect: "postgres",
-    operatorsAliases: false,
-    define: {
-      underscored: true
-    }
-  };
-}
-const sequelize = new Sequelize(connection);
+// let connection;
+// if (process.env.NODE_ENV === 'production') {
+//   console.log("In production!");
+//   connection = process.env.DATABASE_URL;
+// } else {
+//   console.log("Not in production!");
+//   connection = {
+//     database: "horology_db",
+//     dialect: "postgres",
+//     operatorsAliases: false,
+//     define: {
+//       underscored: true
+//     }
+//   };
+// }
+// const sequelize = new Sequelize(connection);
+
+const sequelize = new Sequelize({
+  database: "dcoum4kfftsbu3",
+  username: "pequvxxkeapnyh",
+  password: "bc6389e04c51e90ed0642533360ff9bd5c42dc62c5edbd9733f2f3e306d9d0c3",
+  host: "ec2-54-163-246-159.compute-1.amazonaws.com"
+})
 
 const Watch = sequelize.define("watches", {
   name: Sequelize.STRING,
