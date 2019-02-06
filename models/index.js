f0f03669f464a1c35efe0b8629ee9fc05d4b9b2b
Strangebrewer <BKAShambala@gmail.com>
2019-02-06 03:22:20 -0700

    wtf

--- models/index.js
+++ models/index.js
@@ -7,6 +7,13 @@ var basename  = path.basename(__filename);
 var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config.json')[env];
 var db        = {};
+const { Client } = require('pg');
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable], config);
