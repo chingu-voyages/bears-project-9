11ca54c364d5a6296bbba4c49c345688ad808a6d
Strangebrewer <BKAShambala@gmail.com>
2019-02-06 20:03:09 -0700

    trying to get heroku to connect to proivisioned database rather than my local one.

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = "http://localhost:3001";
+// const BASE_URL = "http://localhost:3001";
+const BASE_URL = "";
 
 export const API = {
   login: function (userData) {
