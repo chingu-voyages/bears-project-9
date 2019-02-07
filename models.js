61ecf7ec023897ea0b27dfdcdcbaee0a97a7c7ad
Strangebrewer <BKAShambala@gmail.com>
2019-02-06 20:38:34 -0700

    trying to get heroku to connect to proivisioned database rather than my local one.

--- models.js
+++ models.js
@@ -3,10 +3,10 @@ const { Sequelize } = require("sequelize");
 // console.log(process.env);
 
 let connection;
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
   console.log(process.env.NODE_ENV)
   connection = process.env.DATABASE_URL;
-} 
+// } 
 // else {
 //   connection = {
 //     database: "horology_db",
