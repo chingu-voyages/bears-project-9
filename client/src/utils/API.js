6ebcdb2d8fcd7560802aab22e0d761a41e5adc6e
Strangebrewer <BKAShambala@gmail.com>
2019-02-06 20:29:54 -0700

    trying to get heroku to connect to proivisioned database rather than my local one.

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,62 +1,62 @@
 import axios from 'axios';
 
-const BASE_URL = REACT_APP_BASE_URL ? REACT_APP_BASE_URL : "";
+const BASE_URL = REACT_APP_BASE_URL;
 
 export const API = {
   login: function (userData) {
-    return axios.post(`${BASE_URL}/users/login`, userData);
+    return axios.post(`${BASE_URL}users/login`, userData);
   },
 
   signup: function (userData) {
-    return axios.post(`${BASE_URL}/users`, userData);
+    return axios.post(`${BASE_URL}users`, userData);
   },
 
   currentuser: function (headers) {
-    return axios.get(`${BASE_URL}/currentuser`, { ...headers });
+    return axios.get(`${BASE_URL}currentuser`, { ...headers });
   },
 
   updateUser: function (id, userData) {
-    return axios.put(`${BASE_URL}/users/${id}`, userData)
+    return axios.put(`${BASE_URL}users/${id}`, userData)
   },
 
   getWatches: function () {
-    return axios.get(`${BASE_URL}/watches`);
+    return axios.get(`${BASE_URL}watches`);
   },
 
   getWatch: function () {
-    return axios.get(`${BASE_URL}/watches/:id`);
+    return axios.get(`${BASE_URL}watches/:id`);
   },
 
 
   adminGetUsers: function (headers) {
-    return axios.get(`${BASE_URL}/admin/users`, { ...headers });
+    return axios.get(`${BASE_URL}admin/users`, { ...headers });
   },
 
   adminCreateUser: function (userData, headers) {
-    return axios.post(`${BASE_URL}/admin/users`, userData, { ...headers })
+    return axios.post(`${BASE_URL}admin/users`, userData, { ...headers })
   },
 
   adminUpdateUser: function (id, userData, headers) {
-    return axios.put(`${BASE_URL}/admin/users/${id}`, userData, { ...headers })
+    return axios.put(`${BASE_URL}admin/users/${id}`, userData, { ...headers })
   },
 
   adminDeleteUser: function (id, headers) {
-    return axios.delete(`${BASE_URL}/admin/users/${id}`, { ...headers });
+    return axios.delete(`${BASE_URL}admin/users/${id}`, { ...headers });
   },
 
   adminCreateWatch: function (watchData, headers) {
-    return axios.post(`${BASE_URL}/admin/watch`, watchData, { ...headers });
+    return axios.post(`${BASE_URL}admin/watch`, watchData, { ...headers });
   },
 
   adminUpdateWatch: function (id, watchData, headers) {
-    return axios.put(`${BASE_URL}/admin/watch/${id}`, watchData, { ...headers });
+    return axios.put(`${BASE_URL}admin/watch/${id}`, watchData, { ...headers });
   },
 
   adminDeleteWatch: function (id, headers) {
-    return axios.delete(`${BASE_URL}/admin/watch/${id}`, { ...headers });
+    return axios.delete(`${BASE_URL}admin/watch/${id}`, { ...headers });
   },
 
   adminRemoveImage: function (id, watchData, headers) {
-    return axios.put(`${BASE_URL}/admin/image/${id}`, watchData, { ...headers });
+    return axios.put(`${BASE_URL}admin/image/${id}`, watchData, { ...headers });
   },
 }
