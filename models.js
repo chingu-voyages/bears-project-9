db9c84d2eb5ef99385c406ba41a656cf808ce19f
Strangebrewer <BKAShambala@gmail.com>
2019-02-09 12:26:43 -0700

    adding requested changes.

--- models.js
+++ models.js
@@ -1,12 +1,9 @@
-const dotenv = require("dotenv");
 const { Sequelize } = require("sequelize");
-
-dotenv.config();
 
 let connection;
 if (process.env.NODE_ENV === "production")
   connection = process.env.DATABASE_URL;
-else if (process.env.NODE_ENV === "development")
+else
   connection = {
     database: "horology_db",
     dialect: "postgres",
