a89b3538aab23ab6846b52f525eb05a950a9f011
spicybyte <onespicybyte@gmail.com>
2019-02-10 12:02:36 -0600

    Add loading modal feature on image upload
    Loading modal prevents manual close of the modal during upload

--- client/src/components/AdminElements/WatchTable.jsx
+++ client/src/components/AdminElements/WatchTable.jsx
@@ -1,19 +1,21 @@
-import React, { PureComponent, Fragment } from 'react';
+import React, { PureComponent, Fragment } from "react";
 import ReactTable from "react-table";
 import Spinner from "../Spinner/Spinner";
 import { API } from "../../utils/API";
 import "react-table/react-table.css";
 import "./Tables.scss";
 
-const CLOUDINARY_URL = `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUD_NAME}/image/upload/`
+const CLOUDINARY_URL = `https://api.cloudinary.com/v1_1/${
+  process.env.REACT_APP_CLOUD_NAME
+}/image/upload/`;
 
 export class WatchTable extends PureComponent {
   state = {
-    data: '',
+    data: "",
     imageUrl: "",
     loading: false,
     watchData: this.props.watchData
-  }
+  };
 
   // buildHeaders = () => {
   //   const token = localStorage.getItem('token');
@@ -23,11 +25,11 @@ export class WatchTable extends PureComponent {
   handleInputChange = event => {
     const { name, value } = event.target;
     this.setState({ [name]: value });
-  }
+  };
 
   updateRow = async row => {
     this.setState({ loading: true });
-    const { brand, description, gender, name, price, id, } = row.original;
+    const { brand, description, gender, name, price, id } = row.original;
     const updateObject = {};
     updateObject.brand = brand;
     updateObject.description = description;
@@ -38,21 +40,19 @@ export class WatchTable extends PureComponent {
     await API.adminUpdateWatch(id, updateObject, headers);
     const watchData = await this.props.fetchWatches();
     setTimeout(() => this.setState({ watchData, loading: false }), 500);
-  }
+  };
 
   deleteWatchModal = id => {
     this.props.setModal({
-      body: (
-        <h2>Are you sure you want to delete this watch?</h2>
-      ),
+      body: <h2>Are you sure you want to delete this watch?</h2>,
       buttons: (
         <Fragment>
           <button onClick={() => this.deleteWatch(id)}>Yes, delete it</button>
           <button onClick={this.props.closeModal}>Cancel</button>
         </Fragment>
       )
-    })
-  }
+    });
+  };
 
   deleteWatch = async id => {
     this.setState({ loading: true });
@@ -63,7 +63,7 @@ export class WatchTable extends PureComponent {
       await this.setState({ watchData, loading: false });
       this.props.closeModal();
     }, 500);
-  }
+  };
 
   imageModal = row => {
     const { brand, name, id, image, publicId } = row.original;
@@ -72,21 +72,25 @@ export class WatchTable extends PureComponent {
       body: <img src={image} alt={`${brand} - ${name}`} />,
       buttons: (
         <Fragment>
-          <button onClick={() => this.adminRemoveImage(id, { publicId: publicId })}>Delete</button>
+          <button
+            onClick={() => this.adminRemoveImage(id, { publicId: publicId })}
+          >
+            Delete
+          </button>
           <button onClick={this.props.closeModal}>Close</button>
         </Fragment>
       )
-    })
-  }
+    });
+  };
 
   handleSelectImage = async event => {
     const files = event.target.files;
-    if (!files[0].type.includes('image')) return;
+    if (!files[0].type.includes("image")) return;
     const data = new FormData();
     await data.append("file", files[0]);
     await data.append("upload_preset", "horology");
     this.setState({ data });
-  }
+  };
 
   saveImageToCloud = async () => {
     const res = await fetch(CLOUDINARY_URL, {
@@ -96,9 +100,22 @@ export class WatchTable extends PureComponent {
     const file = await res.json();
     console.log(file);
     return file;
-  }
+  };
 
   addImageToWatch = async id => {
+    this.props.setModal({
+      body: (
+        <Spinner
+          style={{
+            height: "15px",
+            width: "15px",
+            display: "inline-block"
+          }}
+        />
+      ),
+      loadingModal: true
+    });
+
     if (this.state.data) {
       this.setState({ loading: true });
       const watchImageData = {};
@@ -114,7 +131,7 @@ export class WatchTable extends PureComponent {
       this.setState({ watchData, loading: false });
       this.props.closeModal();
     }
-  }
+  };
 
   adminRemoveImage = async (id, imageData) => {
     this.setState({ loading: true });
@@ -123,7 +140,7 @@ export class WatchTable extends PureComponent {
     const watchData = await this.props.fetchWatches();
     this.setState({ watchData, loading: false });
     this.props.closeModal();
-  }
+  };
 
   uploadImageModal = row => {
     this.props.setModal({
@@ -141,13 +158,15 @@ export class WatchTable extends PureComponent {
       ),
       buttons: (
         <Fragment>
-          <button onClick={() => this.addImageToWatch(row.original.id)}>Upload</button>
+          <button onClick={() => this.addImageToWatch(row.original.id)}>
+            Upload
+          </button>
           <button onClick={this.props.closeModal}>Cancel</button>
         </Fragment>
-      )
-    })
-
-  }
+      ),
+      loadingModal: true
+    });
+  };
 
   // editable react table
   renderEditable = cellInfo => {
@@ -179,49 +198,57 @@ export class WatchTable extends PureComponent {
             width: 140,
             Cell: row => (
               <div className="table-action-icons">
-                {this.state.loading
-                  ? (
-                    <Spinner
-                      style={{
-                        height: "15px",
-                        width: "15px",
-                        display: "inline-block"
-                      }}
-                    />
-                  ) : (
-                    <Fragment>
-                      <button onClick={() => this.updateRow(row)} title="save changes">
-                        <i className="fas fa-save" />
-                      </button>
-                      <button
-                        disabled={row.original.image}
-                        onClick={() => this.uploadImageModal(row)}
-                        title={row.original.image
+                {this.state.loading ? (
+                  <Spinner
+                    style={{
+                      height: "15px",
+                      width: "15px",
+                      display: "inline-block"
+                    }}
+                  />
+                ) : (
+                  <Fragment>
+                    <button
+                      onClick={() => this.updateRow(row)}
+                      title="save changes"
+                    >
+                      <i className="fas fa-save" />
+                    </button>
+                    <button
+                      disabled={row.original.image}
+                      onClick={() => this.uploadImageModal(row)}
+                      title={
+                        row.original.image
                           ? "you must delete the existing image before you can upload another"
-                          : "upload new image"}
-                      >
-                        <i className="fas fa-upload" />
-                      </button>
-                      <button
-                        disabled={!row.original.image}
-                        onClick={() => this.imageModal(row)}
-                        title={!row.original.image
+                          : "upload new image"
+                      }
+                    >
+                      <i className="fas fa-upload" />
+                    </button>
+                    <button
+                      disabled={!row.original.image}
+                      onClick={() => this.imageModal(row)}
+                      title={
+                        !row.original.image
                           ? "there is no image uploaded for this watch"
-                          : "see image"}
-                      >
-                        <i className="fas fa-images" />
-                      </button>
-                      <button onClick={() => this.deleteWatchModal(row.original.id)} title="delete watch">
-                        <i className="fas fa-trash-alt" />
-                      </button>
-                    </Fragment>
-                  )
-                }
-              </ div >
+                          : "see image"
+                      }
+                    >
+                      <i className="fas fa-images" />
+                    </button>
+                    <button
+                      onClick={() => this.deleteWatchModal(row.original.id)}
+                      title="delete watch"
+                    >
+                      <i className="fas fa-trash-alt" />
+                    </button>
+                  </Fragment>
+                )}
+              </div>
             )
           },
           {
-            Header: 'MFG',
+            Header: "MFG",
             accessor: "brand",
             Cell: this.renderEditable
           },
@@ -249,13 +276,12 @@ export class WatchTable extends PureComponent {
             Cell: this.renderEditable
           }
         ]}
-        defaultSorted={
-          [
-            {
-              id: "name",
-              desc: false
-            }
-          ]}
+        defaultSorted={[
+          {
+            id: "name",
+            desc: false
+          }
+        ]}
         defaultPageSize={10}
         className="-striped -highlight"
       />
